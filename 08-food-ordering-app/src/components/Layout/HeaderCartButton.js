c2a64690d273d87fbfa79c2e6402144db4e580b4
Esto Triramdani N <estolagi@gmail.com>
2022-02-14 14:53:56 +0700

    meal

---
+++ 08-food-ordering-app/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import CartIcon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = () => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>5</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
