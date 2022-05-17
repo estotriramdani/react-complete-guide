2d4eb642cb8a6f5a14103bd731aa9682de7df32e
Esto Triramdani Nurlustiawan <enurlustiawan@aio.co.id>
2022-05-17 08:31:50 +0700

    Transition Component

---
+++ 19-react-transition-basic/src/components/Modal/Modal.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Transition from 'react-transition-group/Transition';
+
+import './Modal.css';
+
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
+const modal = (props) => {
+  return (
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
+      {(state) => {
+        const cssClasses = [
+          'Modal',
+          state === 'entering'
+            ? 'ModalOpen'
+            : state === 'exiting'
+            ? 'ModalClosed'
+            : null,
+        ];
+        return (
+          <div className={cssClasses.join(' ')}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
+  );
+};
+
+export default modal;
