eba8d0cbc9ae7d855e4f3a1b27b99ef4e1faa381
Esto Triramdani Nurlustiawan <enurlustiawan@aio.co.id>
2022-05-17 08:52:23 +0700

    animating list

--- 19-react-transition-basic/src/components/Modal/Modal.js
+++ 19-react-transition-basic/src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Transition from 'react-transition-group/Transition';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './Modal.css';
 
@@ -10,31 +10,24 @@ const animationTiming = {
 
 const modal = (props) => {
   return (
-    <Transition
+    <CSSTransition
       in={props.show}
       timeout={animationTiming}
       mountOnEnter
       unmountOnExit
-    >
-      {(state) => {
-        const cssClasses = [
-          'Modal',
-          state === 'entering'
-            ? 'ModalOpen'
-            : state === 'exiting'
-            ? 'ModalClosed'
-            : null,
-        ];
-        return (
-          <div className={cssClasses.join(' ')}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>
-              Dismiss
-            </button>
-          </div>
-        );
+      classNames={{
+        enterActive: 'ModalOpen',
+        enter: 'bg-red',
+        exitActive: 'ModalClosed',
       }}
-    </Transition>
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
